b390c2c5f64def32dfb5fb5dcd6699c69f27a51d
raneemabdulazez <raneem97su@gmail.com>
2023-02-20 20:33:57 -0500

    finished before review

--- lab2/src/components/HornedBeast.js
+++ lab2/src/components/HornedBeast.js
@@ -1,14 +1,59 @@
-import React from "react";
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-class HornedBeast extends React.Component{
-    render(){
-        return(
-            <div>         
-            <h1>{this.props.title}</h1>
-            <img width={500} src={this.props.image_url} />
-            <p>{this.props.description}</p>
-            </div>
- 
+
+class HornedBeast extends React.Component {
+
+    //  when we want to re -redner the compoenent we use setstate 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            favorite: 0
+        }
+    }
+
+
+    addOne = () => {
+        this.setState(
+            {
+                favorite: this.state.favorite + 1
+            }
+        )
+    }
+    render() {
+
+        return (
+
+            <Row xs={1} md={2} className="g-4">
+
+                <Col>
+                    <Card>
+                        <p>{this.state.favorite}</p>
+
+
+                        <Card.Img width={300} onClick={this.addOne} key={this.props._id} width={500} variant="top" src={this.props.image_url} />
+                        <Card.Body>
+                            <Card.Title>{this.props.title} </Card.Title>
+                            <Card.Text>
+                                {this.props.description}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+            </Row>
+
+            // <div>
+            //     <Heart/>
+            // <h1>{this.props.title}</h1>
+            // <img onClick={this.addOne} width ={500}src={this.props.image_url}/>
+            // <p>{this.props.description}</p>   
+
+
+            // </div>
         )
     }
 }
