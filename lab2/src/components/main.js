289aecb24af1ddb2bafcf501707a11671f4d1744
raneemabdulazez <raneem97su@gmail.com>
2023-02-25 20:14:21 -0500

    lab4

--- lab2/src/components/main.js
+++ lab2/src/components/main.js
@@ -1,23 +1,77 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
 import 'bootstrap/dist/css/bootstrap-grid.min.css'
+<<<<<<< HEAD
+
+
+=======
 import Form from 'react-bootstrap/Form';
 
+>>>>>>> d4dcfca (finished lab4)
 class Main extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            data: this.props.data
+            data: this.props.data,
+            updatedData:this.props.data,
         }
     }
 
 
+
+    ChangeNumberOfHorns = (e) => {
+     
+        if (e.target.value){
+        let newHorns = parseInt(e.target.value)
+        
+    
+   
+        console.log("reset data", this.state.data);
+        let updatedHorns = this.state.data.filter(e => e.horns === newHorns ? e : false);
+        console.log("updated Horns ", updatedHorns);
+        
+     this.setState({
+        updatedData:updatedHorns
+     })
+    }
+    
+         else{
+            this.setState({
+                updatedData:this.props.data
+             })
+             
+             console.log("all is working ");
+         }
+
+    }
+
+    
+  
+
+
+
+
+
+
+
+
+
     render() {
+        return (
 
         return (
 
             <div>
+<<<<<<< HEAD
+
+                <div >
+                    {this.state.data.map((element) => (
+                        <HornedBeast key={element._id} title={element.title} width={500} image_url={element.image_url} description={element.description} 
+                       viewBeast={this.props.handlingshow} />
+                        ))}
+                    
+=======
                 <Form onSubmit={this.submitForm}>
                     <Form.Control onClick={this.ChangeNumberOfHorns} name="programming_language" as="select" >
                         <option value="" >all</option>
@@ -38,6 +92,7 @@ class Main extends React.Component {
                         />
                     ))}
 
+>>>>>>> d4dcfca (finished lab4)
                 </div>
             </div>
 
